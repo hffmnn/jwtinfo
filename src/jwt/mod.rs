a04f7935c995e5d3b7fcebff6f3d4baa89b44e2d
sabalsamo <sabalsamo@vectra.ai>
2020-03-21 09:27:32 +0000

    refactor-tests/ moving test inside the module

--- src/jwt/mod.rs
+++ src/jwt/mod.rs
@@ -142,4 +142,13 @@ pub fn parse_token(token: &str) -> Result<Token, JWTDecodePartError> {
 }
 
 #[cfg(test)]
-mod test;
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_assert_parse_token_successfully() {
+        let token = String::from("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmb28iOiJiYXIifQ.dtxWM6MIcgoeMgH87tGvsNDY6cHWL6MGW4LeYvnm1JA");
+        let jwt_token = parse_token(&token);
+        assert_eq!(String::from("{\"foo\":\"bar\"}"), jwt_token.unwrap().body);
+    }
+}
